26c2fa39ee8203fce00ff7390d4ecf7dadb65b57
ilyaBykonya <illya.aleksandrowich@mail.ru>
2020-04-06 13:35:48 +0300

    Добавили немного кода

---
+++ const.h
@@ -0,0 +1,8 @@
+#ifndef
+#define
+
+
+const double Avogadro = 6.02e23;
+const double PI = 3.1415;
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <QCoreApplication>
+#include "const.h"
+
+void print(/* arguments */) {
+  /* code */
+  qDebug() << "Function work";
+}
 
 int main(int argc, char *argv[])
 {
